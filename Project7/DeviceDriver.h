161de10c62c92c6d2382a1d9d123bf72942c905f
changhyun-cra <hellow6044@gmail.com>
2024-07-05 09:13:15 +0900

    initial commit

---
+++ Project7/DeviceDriver.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "FlashMemoryDevice.h"
+#include <stdexcept>
+
+class DeviceDriver
+{
+public:
+    DeviceDriver(FlashMemoryDevice* hardware);
+    int read(long address);
+    void write(long address, int data);
+
+protected:
+    FlashMemoryDevice* m_hardware;
+};
+
+class ReadFailException : public std::exception {
+
+};
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../Project7/DeviceDriver.cpp"
+
+using namespace testing;
+
+TEST(TestCaseName, TestName) {
+	EXPECT_THAT(1, Eq(1));
+}
